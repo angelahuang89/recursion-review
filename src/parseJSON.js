882c7e37a0ce624a4abdd1de055df19eec19007f
angelahuang89 <angelahuang89@gmail.com>
2017-10-10 18:41:06 -0700

    Work on primitives and nested arrays for parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,34 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  //number
+  if (!isNaN(Number(json))) {
+    return Number(json);
+  }
+  //string
+  var arr = json.split('');
+  if ( arr[0] === '\"') {
+    return arr.slice( 1, arr.length - 1 ).join('');
+  }
+  //boolean
+  if (json === 'true') {
+    return true;
+  }
+  if (json === 'false') {
+    return false;
+  }
+  //null
+  if (json === 'null') {
+    return null;
+  }
+  //array
+  if ( arr[0] === '[') {
+    var arrSpl = arr.slice( 1, arr.length - 1 ).join('');
+    var result = arrSpl.split(',');
+    for (var i = 0; i < result.length; i++) {   
+      result[i] = parseJSON(result[i]);
+    }
+    return result;
+  }   
+  //object
 };
